960bfabbad893ac4fa7161479b573430ed443902
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2021-09-29 12:10:36 +0530

    Added base code for linear regression.

---
+++ src/Classification/LinearRegression.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd 
+
+"""
+Implementation of Multivariate Linear Regression
+"""
+class LinearRegression:
+    """
+    Constructor
+    """
+    def __init__(self):
+        pass 
+
+    """
+    Closed Form Solution
+    """
+    def getParametersClosedForm(self):
+        pass 
+
+    """
+    Gradient Descent Solution
+    """
+    def getParametersGradientDescent(self,learningRate,numInterations,decay):
+        pass 
+    
+    """
+    Newton's Method Solution
+    """
+    def getParametersNewtonMethod(self,numIterations):
+        pass 
+
+    
---
+++ src/Classification/RegressionHelpers.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd 
+
+"""
+Functions to aid in Regression Calculation and Analysis
+"""
+
+def getMeanSquaredError(train_data,parameters,test_data):
+    pass 
+
+def dataFrameToNumpyArray(dataFrame):
+    pass 
