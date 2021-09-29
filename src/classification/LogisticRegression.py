a87039ac479b278aee7dc7be30afdb45d24c8f13
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2021-09-30 05:29:13 +0530

    Added code for Gradient Descent for Logistic Regression.

--- src/classification/LogisticRegression.py
+++ src/classification/LogisticRegression.py
@@ -1,17 +1,54 @@
 import pandas as pd 
 import numpy as np 
-
+from ClassificationHelpers import appendNumberToEveryRow,reshapeVector, sigmoid
 
 """
 Implementation of the Logistic Regression Algorithm
 """
 class LogisticRegression:
 
-    def __init__(self):
-        pass 
+    """
+    Constructor
+    """
+    def __init__(self,x,y):
+        self._x = np.array(x,dtype='float') 
+        self._y = np.array(y,dtype='float') 
+        self._numRecords = self._x.shape[0]
 
-    def getParametersGradientDescent(self):
-        pass 
+    """
+    Closed Form Solution
+    """
+    def getParametersGradientDescent(self,learningRate = 0.00001,numInterations = 1000,decay = 0):
+        # Converting the input into suitable shapes
+        x = appendNumberToEveryRow(self._x,1)
+        y = reshapeVector(self._y,(self._numRecords,1))
+        n = self._numRecords
+        
+        # Performing the computation
+        numFeatures = x.shape[1]
+        weights = np.zeros((numFeatures,1))
+
+        for _ in range(numInterations):
+
+            # Step 1
+            calc1 = np.dot(x,weights)
+
+            # Step 2
+            calc2 = sigmoid(calc1)
+
+            # Step 3 
+            calc3 = np.dot(x.T, (calc2 - y))
+
+            # Step 4 
+            weights -= (-1.0/n) * learningRate * calc3 
+
+            # Step 5 
+            learningRate -= decay
+        
+        return weights
+        
+
+
 
     def getParameterGradientAscent(self):
         pass 
