73a7cfb2d728269fc00f22c9d963d169373f1a89
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2021-09-30 23:32:37 +0530

    Added starter code for Naive Bayes.

---
+++ src/classification/NaiveBayes.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+import numpy as np 
+
+"""
+Implementation of the Gaussian Naive Bayes Classifier
+"""
+class NaiveBayes:
+    """
+    Constructor
+    """
+    def __init__(self):
+        pass 
+
+    """
+    Get Predicted Class Labels 
+    """
+    def getClassLabels(self):
+        pass 
+
+    """
+    Get Accuracy
+    """
+    def getAccuracy(self):
+        pass 
