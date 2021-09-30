Fixed Boolean Masking Issue in Naive Bayes Algorithm.

src/classification/NaiveBayes.py
@@ -1,17 +1,19 @@
 import pandas as pd 
 import numpy as np 
-from ClassificationHelpers import getGaussianLogVector,getMeanOverArray,getVarianceOverArray
+from ClassificationHelpers import getGaussianLogVector,getMeanOverArray,getVarianceOverArray,reshapeVector
 
 """
 Implementation of the Gaussian Naive Bayes Classifier
 """
+
 class NaiveBayes:
     """
     Constructor
     """
     def __init__(self,x,y):
-        self._x = np.array(x) 
-        self._y = np.array(y) 
+        self._x = np.array(x)
+        self._y = np.array(y)
+        self._y = reshapeVector(self._y,(self._x.shape[0],1))  
 
         self._numRecords = self._x.shape[0]
         self._numFeatures = self._x.shape[1]
@@ -32,13 +34,20 @@ class NaiveBayes:
         m = self._numFeatures
         k = self._numClasses
 
+       
         probabilityValues = np.zeros((n,k))
 
         for classLabel in range(k):
             classId = str(classLabel)
 
             # Step 1 
-            calc1 = x[y == classLabel]
+            calc1 = []
+            for i in range(len(x)):
+              if y[i] == classLabel:
+                calc1.append(x[i])
+            
+            calc1 = np.array(calc1)
+            print(calc1.shape)
 
             # Step 2
             self._meanValues[classId] = getMeanOverArray(calc1,0)
@@ -51,6 +60,7 @@ class NaiveBayes:
 
             # Step 5 
             calc2 = getGaussianLogVector(x,self._meanValues[classId],self._varianceValues[classId])
+            print(calc2.shape)
 
             # Step 6 
             calc3 = calc2 + np.log(self._likelihoodValues[classId])
@@ -65,4 +75,4 @@ class NaiveBayes:
     Get Accuracy
     """
     def getAccuracy(self):
-        pass 
+        pass