b0cdf058544f4719ae3d19067e062d38569f1134
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2021-10-02 05:26:35 +0530

    Added split functionality for dataset.

---
+++ src/helpers/DataMethods.py
@@ -0,0 +1,20 @@
+"""
+Helper functions to help in data manipulation
+"""
+
+"""
+Pandas implementation of train_test_split
+"""
+
+def getTrainingTestingData(data, trainingRatio = 0.75, seed = 42):
+
+    # Step 1
+    trainingData = data.sample(frac = trainingRatio, random_state = seed)
+
+    # Step 2 
+    trainingIndex = trainingData.index 
+
+    # Step 3 
+    testingData = data.drop(trainingIndex)
+
+    return trainingData,trainingIndex
